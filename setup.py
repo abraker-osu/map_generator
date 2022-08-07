1739bb72c5aba334605a5cd7a76797d69ba250e2
abraker <abraker95@gmail.com>
2022-08-07 19:47:35 -0400

    add setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='OsuMapGenerator',
+    version='v20220807',
+    description='Library to generate osu! maps programmatically',
+    author='abraker',
+    packages=['OsuMapGenerator'],
+)
